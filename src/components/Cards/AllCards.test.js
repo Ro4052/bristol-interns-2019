dceac991904ba0c09443274a86dd0bd44268b8c0
Sibela Chinareva <schinareva@scottlogic.com>
2019-07-03 11:41:51 +0100

    Added tests for cards

--- src/components/Cards/AllCards.test.js
+++ src/components/Cards/AllCards.test.js
@@ -1,9 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import AllCards from './AllCards';
+import {AllCards} from './AllCards';
+import { shallow, mount } from 'enzyme';
 
-it('renders without crashing', () => {
-//     const div = document.createElement('div');
-//     ReactDOM.render(<AllCards />, div);
-//     ReactDOM.unmountComponentAtNode(div);
-});
+describe('on initial render', () => {
+    it('renders without crashing', () => {
+        shallow(<AllCards/>);
+    })
+    it("doesn't display all cards", () => {
+        const wrapper = shallow(<AllCards/>);
+        expect(wrapper.exists('#played-cards li')).toEqual(false)
+    })
+})
+
+describe('on given a set of played cards', () => {
+    it('displays the cards currently played', () => {
+        let cards = [
+            {
+                id: 1
+            },
+            {
+                id: 2
+            },
+            {
+                id: 3
+            },
+        ]
+        const wrapper = shallow(<AllCards cards={cards}/>);
+        expect(wrapper.find('ul').children().length).toEqual(3);
+    })
+})
+
