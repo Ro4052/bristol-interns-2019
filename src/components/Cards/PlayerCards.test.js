d57165fd56a7d7e2485a6049169b1a4e2f2ffe2d
Sibela Chinareva <schinareva@scottlogic.com>
2019-07-03 15:57:36 +0100

    Made sure tests pass

--- src/components/Cards/PlayerCards.test.js
+++ src/components/Cards/PlayerCards.test.js
@@ -4,10 +4,10 @@ import { shallow, mount } from 'enzyme';
 
 describe('on initial render', () => {
     it('renders without crashing', () => {
-        shallow(<PlayerCards/>);
+        shallow(<PlayerCards fetchCards={() => {}} playCard={() => {}}/>);
     })
     it("doesn't display all cards", () => {
-        const wrapper = shallow(<PlayerCards/>);
+        const wrapper = shallow(<PlayerCards fetchCards={() => {}} playCard={() => {}}/>);
         expect(wrapper.exists('#my-cards li')).toEqual(false)
     })
 })
@@ -20,27 +20,24 @@ const helper = {
 
 
 describe('on given a set of cards', () => {
-    let playerCards = [1, 2, 3]
-    let currentPlayer = {
-        cards: playerCards
-    }
-    // it('displays the correct number of cards', () => {
-    //     const wrapper = shallow(<PlayerCards currentPlayer={currentPlayer}/>);
-    //     expect(wrapper.find('ul').children().length).toEqual(3);
-    // })
-    // it('displays the correct card images', () => {
-    //     const wrapper = shallow(<PlayerCards currentPlayer={currentPlayer}/>);
-    //     console.log(wrapper.find('ul').children());
+    let playerCards = [1, 2, 3];
+    it('displays the correct number of cards', () => {
+        const wrapper = shallow(<PlayerCards cards={playerCards} fetchCards={() => {}} playCard={() => {}}/>);
+        expect(wrapper.find('ul').children().length).toEqual(3);
+    })
+    it('displays the correct card images', () => {
+        const wrapper = shallow(<PlayerCards cards={playerCards} fetchCards={() => {}} playCard={() => {}}/>);
+        console.log(wrapper.find('ul').children());
         
-    //     expect(wrapper.find('ul').children().length).toEqual(3);
-    // })
-    // it('is able to play a chosen card', () => {
-    //     const mockedEvent = { target: {id: '1'} }
-    //     const spy = jest.spyOn(PlayerCards.prototype, 'playCard');
-    //     const wrapper = shallow(<PlayerCards currentPlayer={currentPlayer}/>);
-    //     wrapper.find('#card-1').simulate('click', mockedEvent);
-    //     expect(spy).toHaveBeenCalled();
-    //     spy.mockRestore();
-    // })
+        expect(wrapper.find('ul').children().length).toEqual(3);
+    })
+    it('is able to play a chosen card', () => {
+        const mockedEvent = { target: {id: '1'} }
+        const spy = jest.spyOn(PlayerCards.prototype, 'playCard');
+        const wrapper = shallow(<PlayerCards cards={playerCards} fetchCards={() => {}} playCard={() => {}}/>);
+        wrapper.find('#card-1').simulate('click', mockedEvent);
+        expect(spy).toHaveBeenCalled();
+        spy.mockRestore();
+    })
 })
 
