5ae51b1ae769b0d165162737d1ef36b6ebe42701
Matilda Woodfield <mwoodfield@scottlogic.com>
2019-07-04 11:08:02 +0100

    Fixed linting errors

--- server/cards.js
+++ server/cards.js
@@ -1,17 +1,17 @@
 module.exports.assign = (users, number_of_cards) => {
-    var allCards = getAllCurrentCards(users);
-    var cardsSet = [];
+    const allCards = getAllCurrentCards(users);
+    let cardsSet = [];
 
-    for (var j = 0; j < number_of_cards; j++) {
-        var min = 1;
-        var max = 50;
-        var random = Math.floor(Math.random() * (max - min) + min);
-        var availableGlobally = allCards.indexOf(random) < 0;
-        var inCurrentHand = cardsSet.indexOf(random) >= 0;
+    for (let j = 0; j < number_of_cards; j++) {
+        const min = 1;
+        const max = 50;
+        const random = Math.floor(Math.random() * (max - min) + min);
+        const availableGlobally = allCards.indexOf(random) < 0;
+        const inCurrentHand = cardsSet.indexOf(random) >= 0;
         while (!availableGlobally || inCurrentHand) {
-            random = Math.floor(Math.random() * (max - min) + min);
-            available = allCards.indexOf(random) < 0;
-            inCurrentHand = cardsSet.indexOf(random) >= 0;
+            const random = Math.floor(Math.random() * (max - min) + min);
+            const available = allCards.indexOf(random) < 0;
+            const inCurrentHand = cardsSet.indexOf(random) >= 0;
         }
         cardsSet.push(random);
     }
@@ -20,9 +20,9 @@ module.exports.assign = (users, number_of_cards) => {
 }
 
 function getAllCurrentCards(users) {
-    all = []
-    for (var i = 0; i < users.length; i++) {
-        for (var j = 0; j < users[i].cards.length; j++) {
+    let all = [];
+    for (let i = 0; i < users.length; i++) {
+        for (let j = 0; j < users[i].cards.length; j++) {
             all.push(users[i].cards[j]);
         }
     }    
