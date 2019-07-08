443628d72316fb5d11b233a444b60d7064ef8ae8
Matilda Woodfield <mwoodfield@scottlogic.com>
2019-07-08 17:36:16 +0100

    Started setting up code coverage

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -1,5 +1,3 @@
-/// <reference types="Cypress" />
-
 describe('The Dashboard Page', function () {
     it('logs in programmatically without using the UI', () => {
       // destructuring assignment of the this.currentUser object
--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,5 +1,5 @@
 describe('The Home Page', function() {
     it('successfully loads', function() {
-      cy.visit('http://localhost:8080') // change URL to match your dev URL
+      cy.visit('http://localhost:1234') // change URL to match your dev URL
     })
 })
