817ae930723a23236d6dd8441f143fbf59f4146c
Matilda Woodfield <mwoodfield@scottlogic.com>
2019-07-08 13:41:36 +0100

    Rename

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, combineReducers  } from 'redux';
 import reducer from './reducer';
-import playerReducer from './playerReducer';
+import playerReducer from './playerReduce';
 import thunk from 'redux-thunk';
 
 const store = createStore(
