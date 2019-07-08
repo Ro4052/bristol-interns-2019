b884cc6b06689ad4b55b291426efa45c5cc6928b
Isabelle Atkins <iatkins@scottlogic.com>
2019-07-08 15:43:57 +0100

    e2e login

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -3,3 +3,24 @@ describe('The Home Page', function() {
       cy.visit('/') // change URL to match your dev URL
     })
 })
+describe('The Login Page', function () {
+  beforeEach(function () {
+  })
+
+  it('sets auth cookie when logging in via form submission', function () {
+    // destructuring assignment of the this.currentUser object
+    const username = 'jane'
+
+    cy.get('input').type(username)
+    cy.get('button').click() 
+
+    // we should be redirected to /dashboard
+    cy.url().should('include', '/dashboard')
+
+    // our auth cookie should be present
+    cy.getCookie('username').should('exist')
+
+    // UI should reflect this user being logged in
+    cy.get('#players').should('contain', 'jane')
+  })
+})
