47c7b04f1402b63b24212b262d8ddf52dd8f0c9e
Isabelle Atkins <iatkins@scottlogic.com>
2019-07-09 16:29:45 +0100

    Added end turn on after block

--- cypress/integration/auth_spec.js
+++ cypress/integration/auth_spec.js
@@ -1,22 +1,31 @@
 // if authorised, should be redirected to the dashboard
-
-describe('No auth cookie', function () {
-    it('stops you getting to /dashboard', function () {
-        cy.visit('/dashboard');
-        // we should not be redirected to /dashboard
-        cy.url().should('eq', Cypress.config().baseUrl);
-    }); 
-});
-
-describe('auth cookie', function () {
-    beforeEach(function() {
-        cy.visit('/');
-        const username = 'jane'
-        cy.get('input').type(username)
-        cy.get('button').click() 
+describe('The Auth Page', function () {
+    describe('No auth cookie', function () {
+        it('stops you getting to /dashboard', function () {
+            cy.visit('/dashboard');
+            // we should not be redirected to /dashboard
+            cy.url().should('eq', Cypress.config().baseUrl);
+        }); 
     });
-    it('sends you to dashboard', function() {
-        cy.visit('/dashboard');
-        cy.url().should('include', '/dashboard');
+
+    describe('auth cookie', function () {
+        beforeEach(function() {
+            cy.visit('/');
+            const username = 'jane'
+            cy.get('input').type(username)
+            cy.get('button').click() 
+        });
+        
+        afterEach(() => {
+            cy.request({
+                url: '/api/end',
+                method: 'GET'
+            });
+            cy.visit('/')
+        })
+        it('sends you to dashboard', function() {
+            cy.visit('/dashboard');
+            cy.url().should('include', '/dashboard');
+        });
     });
 });
--- cypress/integration/playedCards_spec.js
+++ cypress/integration/playedCards_spec.js
@@ -1,31 +1,31 @@
-describe('logs in and plays card', function() {
-    before(() => {
-        cy.request({
-            url: '/auth/login',
-            method: 'POST',
-            body: {
-                username: 'jane'
-            }
-        });
-        cy.request('http://localhost:8081/connect?url=http://localhost:8080')
+// describe('logs in and plays card', function() {
+//     before(() => {
+//         cy.request({
+//             url: '/auth/login',
+//             method: 'POST',
+//             body: {
+//                 username: 'jane'
+//             }
+//         });
+//         cy.request('http://localhost:8081/connect?url=http://localhost:8080')
 
-        // tell the http server at 8081 to send a message
-        cy.request('http://localhost:8081/message?m=hello')
+//         // tell the http server at 8081 to send a message
+//         cy.request('http://localhost:8081/message?m=hello')
 
-        // tell the http server at 8081 to disconnect
-        cy.request('http://localhost:8081/disconnect')
-    })
+//         // tell the http server at 8081 to disconnect
+//         cy.request('http://localhost:8081/disconnect')
+//     })
 
-    after(() => {
-        cy.request({
-            url: '/api/end',
-            method: 'GET'
-        });
-    })
-    it('gives list of played cards', function() {
-        cy.visit('http://localhost:8080/dashboard')
+//     after(() => {
+//         cy.request({
+//             url: '/api/end',
+//             method: 'GET'
+//         });
+//     })
+//     it('gives list of played cards', function() {
+//         cy.visit('http://localhost:8080/dashboard')
     
-        console.log(cy.get('#played-cards'))
-    })
-})
-des
+//         console.log(cy.get('#played-cards'))
+//     })
+// })
+// des
