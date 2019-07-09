161e9c18651c7dd5773046e0c8aef6f6d9e0711b
Isabelle Atkins <iatkins@scottlogic.com>
2019-07-09 16:22:25 +0100

    Spaces

--- cypress/integration/auth_spec.js
+++ cypress/integration/auth_spec.js
@@ -1,22 +1,22 @@
 // if authorised, should be redirected to the dashboard
 
-
 describe('No auth cookie', function () {
-  it('stops you getting to /dashboard', function () {
-    cy.visit('/dashboard') 
-
-
-    // we should not be redirected to /dashboard
-    cy.url().should('eq', Cypress.config().baseUrl)
-  }); 
-})
+    it('stops you getting to /dashboard', function () {
+        cy.visit('/dashboard');
+        // we should not be redirected to /dashboard
+        cy.url().should('eq', Cypress.config().baseUrl);
+    }); 
+});
 
 describe('auth cookie', function () {
-  beforeEach(function() {
-    cy.request('POST', '/auth/login', {username: 'jane'})
-  })
-  it('sends you to dashboard', function() {
-    cy.visit('/dashboard')
-    cy.url().should('include', '/dashboard')
-  })
-})
+    beforeEach(function() {
+        cy.visit('/');
+        const username = 'jane'
+        cy.get('input').type(username)
+        cy.get('button').click() 
+    });
+    it('sends you to dashboard', function() {
+        cy.visit('/dashboard');
+        cy.url().should('include', '/dashboard');
+    });
+});
