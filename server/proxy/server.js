d37fe5867a511be4ed1d61be59b45fae042cb638
Sibela Chinareva <schinareva@scottlogic.com>
2019-07-11 11:05:17 +0100

    Added cookie jar to axios

--- server/proxy/server.js
+++ server/proxy/server.js
@@ -1,13 +1,18 @@
-const client = require('socket.io-client')
-const express = require('express')
+const client = require('socket.io-client');
+const express = require('express');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const axios = require('axios')
-const app = express()
+const axios = require('axios').default;
+const axiosCookieJarSupport = require('axios-cookiejar-support').default;
+const tough = require('tough-cookie');
+ 
+axiosCookieJarSupport(axios);
+ 
+const cookieJar = new tough.CookieJar();
+const app = express();
 
-let socket
-let cards;
+let socket;
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -21,41 +26,41 @@ app.get('/connect-and-play', (req, res) => {
     const instance = axios.create({
         baseURL: url,
         timeout: 500,
+        jar: cookieJar,
+        withCredentials: true
     });
     
     socket.on('connect', () => {
         instance.post('auth/login', { 
             username: "halfling" 
         })
-        .then(() => {
-            return instance.get('api/cards')
-        })
         .then((response) => {
-            cards = response.data;
-            return instance.get('api/start')
+            if (response.status === 200) {
+                return instance.get('api/cards')
+            }
         })
-        .then((startResponse) => {
+        .then((response) => Promise.all([
+            response.data,
+            instance.get('api/start')
+        ]))
+        .then(([cards, startResponse]) => {
             if (startResponse.status === 200) {
                 return instance.post('api/playCardWord', {
                     card: cards[0].id.toString(),
                     word: "Hello"
-                })
+                });
             }
         })
-        .then(() => {
-            res.sendStatus(200);
-        })
-        .catch((err) => {
-            console.error(err);
-        });
+        .then(() => res.sendStatus(200))
+        .catch((err) => console.error(err));
     })
 })
 
 app.get('/disconnect', (req, res) => {
     socket.on('disconnect', () => {
-        res.sendStatus(200)
-    })
-    socket.disconnect()
+        res.sendStatus(200);
+    });
+    socket.disconnect();
 })
 
-app.listen(8081, () => console.log('Server running on port: 8081'))
+app.listen(8081, () => console.log('Server running on port: 8081'));
