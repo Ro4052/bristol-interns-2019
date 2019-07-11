ba1b92d33088972ea9031026d459a4f52354fa45
Matilda Woodfield <mwoodfield@scottlogic.com>
2019-07-11 09:14:00 +0100

    Fixed Auth unit test

--- src/components/Auth/Auth.test.js
+++ src/components/Auth/Auth.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { shallow } from 'enzyme';
-import Auth from './Auth';
+import { Auth } from './Auth';
 import PlayCard from '../PlayerInfo/PlayCard';
 
 describe('on initial mount', () => {
@@ -9,15 +9,15 @@ describe('on initial mount', () => {
         const wrapper = shallow(<Auth/>);
         wrapper.setState({ loading: true });
         expect(wrapper.type()).toEqual(null);
-    })
+    });
     it('redirects if stopped loading and unauthenticated', () => {
         const wrapper = shallow(<Auth/>);
         wrapper.setState({ loading: false, authenticated: false });
         expect(wrapper.find(Redirect).length).toEqual(1);
-    })
+    });
     it('renders the component if stopped loading and authenticated', () => {
         const wrapper = shallow(<Auth render={() => <PlayCard/>}/>);
         wrapper.setState({ loading: false, authenticated: true });
         expect(wrapper.find(PlayCard).length).toEqual(1);
-    })
-})
+    });
+});
