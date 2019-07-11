f2fb9a8b500ba4a27975cf2d0519d8b1ad59b5e9
Matilda Woodfield <mwoodfield@scottlogic.com>
2019-07-11 10:05:01 +0100

    Setup test, need multiple players

---
+++ cypress/integration/play_card_spec.js
@@ -0,0 +1,12 @@
+describe('Play card', () => {
+    beforeEach(() => {
+        cy.login('username');
+        cy.startGame();
+    });
+
+    describe('on word appearing', () => {
+        it('is able to play a card', () => {
+            // TODO: Write test
+        });
+    });
+});
