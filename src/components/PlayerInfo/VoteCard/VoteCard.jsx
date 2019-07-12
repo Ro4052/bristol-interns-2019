b58f68a56c55c3b069f62736893d1886f69c9a0a
Sibela Chinareva <schinareva@scottlogic.com>
2019-07-12 17:22:33 +0100

    Release

--- src/components/PlayerInfo/VoteCard/VoteCard.jsx
+++ src/components/PlayerInfo/VoteCard/VoteCard.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from '../PlayWordAndCard/PlayWordAndCard.module.css';
 
 export const VoteCard = () => {
     return (
         <div>
-            <h3>Vote for a card</h3>
+            <h3 data-cy="vote-card">Vote for a card</h3>
         </div>
     );
 }
