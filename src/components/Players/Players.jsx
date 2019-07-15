20151db1754d411f2011f7d982befeae0ed45e06
Matilda Woodfield <mwoodfield@scottlogic.com>
2019-07-15 15:47:01 +0100

    Moved css into correct component

--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import style from '../Dashboard/Dashboard.module.css';
+import styles from './Players.module.css';
 import { connect } from 'react-redux';
 
 export class Players extends React.PureComponent {
     render() {
         return (
-            <div className={style.currentPlayersBox}>
+            <div className={styles.currentPlayersBox}>
                 <h3 data-cy="players-heading">Players:</h3>
                 <ul id="players" data-cy='players-list'>
                     {this.props.players.map((player, key) => <li key={key}><span data-cy='player-username'>{player.username}</span>: <span data-cy='player-score'>{player.score}</span> points</li>)}
