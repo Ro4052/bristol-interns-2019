b057dc60c8515f0d143f1e5382085104d37634ed
Sibela Chinareva <schinareva@scottlogic.com>
2019-07-16 16:51:07 +0100

    removed a bunch of todos

--- cypress/integration/player_cards_spec.js
+++ cypress/integration/player_cards_spec.js
@@ -4,14 +4,7 @@ describe('The Player Cards Component', () => {
         cy.wait(500);
         cy.startGame();
     });
-
-    describe('before the game has started', () => {
-        it("shouldn't display the cards", () => {
-            // TODO: Comment this back in when we have implemented what it is testing
-            // cy.get('[data-cy="my-cards"]').children().its('length').should('eq', 0);
-        });
-    });
-
+    
     describe('when the game has started', () => {
         it("displays the correct number of cards", () => {
             cy.get('[data-cy="my-cards"]').children().its('length').should('eq', 3);
