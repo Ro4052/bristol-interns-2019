f086534090340af345433ce2feb2e523e7e371f1
Matilda Woodfield <mwoodfield@scottlogic.com>
2019-07-17 15:04:45 +0100

    Fixed failing unit tests

--- src/components/shared/CardList/Card/Card.test.js
+++ src/components/shared/CardList/Card/Card.test.js
@@ -7,14 +7,14 @@ const card1 = { cardId: 1 };
 describe('on enabled', () => {
     it("has the correct className", () => {
         const wrapper = shallow(<Card card={card1} enabled={true}/>);
-        expect(wrapper.find({ 'data-cy': `card-${card1.cardId}` }).prop('className')).toEqual('card enabled');
+        expect(wrapper.find({ 'data-cy': 'card' }).first().prop('className')).toEqual('card enabled');
     });
 });
 
 describe('on not enabled', () => {
     it("has the correct className", () => {
         const wrapper = shallow(<Card card={card1} enabled={false}/>);
-        expect(wrapper.find({ 'data-cy': `card-${card1.cardId}` }).prop('className')).toEqual('card disabled');
+        expect(wrapper.find({ 'data-cy': 'card' }).first().prop('className')).toEqual('card disabled');
     });
 
 });
@@ -23,14 +23,14 @@ describe('on click', () => {
     it('calls the handleClick function', () => {
         const spy = jest.spyOn(Card.prototype, 'handleClick');
         const wrapper = shallow(<Card card={card1} enabled={false} />);
-        wrapper.find({ 'data-cy': `card-${card1.cardId}` }).simulate('click');
+        wrapper.find({ 'data-cy': 'card' }).first().simulate('click');
         expect(spy).toHaveBeenCalled();
         spy.mockRestore();
     });
     it('calls the handleClick function from props', () => {
         const spy = jest.fn();
         const wrapper = shallow(<Card card={card1} enabled={true} handleClick={spy} />);
-        wrapper.find({ 'data-cy': `card-${card1.cardId}` }).simulate('click');
+        wrapper.find({ 'data-cy': 'card' }).first().simulate('click');
         expect(spy).toHaveBeenCalled();
         spy.mockRestore();
     });
