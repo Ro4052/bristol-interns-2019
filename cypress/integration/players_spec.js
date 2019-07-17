ddc3ca5bd86d92242d60b6cc168104aafa63e4a6
Sibela Chinareva <schinareva@scottlogic.com>
2019-07-17 14:21:16 +0100

    Added a redux mock store in the login test and ensured all tests pass

--- cypress/integration/players_spec.js
+++ cypress/integration/players_spec.js
@@ -21,23 +21,44 @@ describe('Players', () => {
 
     describe('on another player log in', () => {
         it('their name is added to the players list', () => {
+            cy.server();
+            cy.route({
+                method: "POST",
+                url: "/auth/login",
+            }).as('login');
             cy.login('player1');
-            cy.request(`http://localhost:12346/connect?url=${encodeURIComponent(Cypress.config().baseUrl)}`)
-            .then(() => {
-                cy.get('[data-cy="player-username"]').first().should('have.text', 'player1');
-                cy.get('[data-cy="player-username"]').last().should('have.text', 'halfling');
+            cy.wait('@login').then(() => {
+                cy.request(`http://localhost:12346/connect?url=${encodeURIComponent(Cypress.config().baseUrl)}`)
+                .then(() => {
+                    cy.get('[data-cy="player-username"]').first().should('have.text', 'player1');
+                    cy.get('[data-cy="player-username"]').last().should('have.text', 'halfling');
+                });
             });
         });
     });
 
     describe('on completing a round', () => {
         it('updates the scores', () => {
+            cy.server();
+            cy.route({
+                method: "POST",
+                url: "/auth/login",
+            }).as('login');
             cy.login('player1');
-            cy.request(`http://localhost:12346/connect?url=${encodeURIComponent(Cypress.config().baseUrl)}`)
-            .then(() => {
-                cy.startGame();
-                cy.request(`http://localhost:12346/playCardWord?url=${encodeURIComponent(Cypress.config().baseUrl)}`)
+            cy.wait('@login').then(() => {
+                cy.request(`http://localhost:12346/connect?url=${encodeURIComponent(Cypress.config().baseUrl)}`)
                 .then(() => {
+                    cy.get('[data-cy="player-username"]').first().should('have.text', 'player1');
+                    cy.get('[data-cy="player-username"]').last().should('have.text', 'halfling');
+                    cy.route({
+                        method: "GET",
+                        url: "/api/start",
+                    }).as('start');
+                    cy.startGame();
+                    cy.wait('@start').then(() => {
+                        cy.get('[data-cy="start-game"]').should('not.exist');
+                        cy.request(`http://localhost:12346/playCardWord?url=${encodeURIComponent(Cypress.config().baseUrl)}`)
+                    });
                     cy.playCard();
                     cy.voteCard();
                     cy.get('[data-cy="player-score"]').first().should('have.text', '2');
