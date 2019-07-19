b4f6e83a57a93a3732881025dda2791e417e5e6b
Isabelle Atkins <iatkins@scottlogic.com>
2019-07-19 09:00:21 +0100

    made monster function not component

--- src/components/Monster/Monster.jsx
+++ src/components/Monster/Monster.jsx
@@ -6,34 +6,33 @@ import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
 
-export class Monster extends React.Component {
-    render() {
+export function Monster(props) {
+
         let bodyClass = styles.body;
-        if (this.props.invalidWord) bodyClass = styles.disappointedbody;
-        if (this.props.winner) bodyClass = styles.victorybody;
-        return (
-            <div className={styles.ufo}>
-                <div className={styles.monster}>
-                    <div className={bodyClass}> 
-                        <div className={styles.ear}></div>
-                        <div className={styles.ear}></div>
-                        <div className={styles.vampimouth}>
-                            <div className={styles.vampitooth}></div>
-                        </div>
-                    
-                        <div className={this.props.invalidWord ? styles.angryeyelid : styles.eyelid}>
-                            <div className={styles.eyes}>
-                                <div className={styles.eye}>
-                                    <div className={cx({ meaneye: this.props.invalidWord})}></div>
-                                    {/* <div className={cx({tear: this.props.error})}></div> */}
-                                </div>
+        if (props.invalidWord) bodyClass = styles.disappointedbody;
+        if (props.winner) bodyClass = styles.victorybody;
+    return (
+        <div className={styles.ufo}>
+            <div className={styles.monster}>
+                <div className={bodyClass}> 
+                    <div className={styles.ear}></div>
+                    <div className={styles.ear}></div>
+                    <div className={styles.vampimouth}>
+                        <div className={styles.vampitooth}></div>
+                    </div>
+                
+                    <div className={props.invalidWord ? styles.angryeyelid : styles.eyelid}>
+                        <div className={styles.eyes}>
+                            <div className={styles.eye}>
+                                <div className={cx({ meaneye: props.invalidWord})}></div>
+                                {/* <div className={cx({tear: props.error})}></div> */}
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );   
 }
 
 const mapStateToProps = (state) => ({
