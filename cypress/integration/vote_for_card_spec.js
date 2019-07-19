ec296cf258f4c532b261a0524c1f9a710062ed0c
Sibela Chinareva <schinareva@scottlogic.com>
2019-07-19 15:10:09 +0100

    Voting card test to what it was before

--- cypress/integration/vote_for_card_spec.js
+++ cypress/integration/vote_for_card_spec.js
@@ -2,11 +2,8 @@ describe('Vote for a card', () => {
     describe('on everyone played cards', () => {
         beforeEach(() => {
             cy.login('unicorn');
-            cy.request({
-                method: 'GET',
-                url: `http://localhost:12346/connect?url=${encodeURIComponent(Cypress.config().baseUrl)}`,
-                followRedirect: false
-            }).then(() => {
+            cy.request(`http://localhost:12346/connect?url=${encodeURIComponent(Cypress.config().baseUrl)}`)
+            .then(() => {
                 cy.startGame();
                 cy.request(`http://localhost:12346/playCardWord?url=${encodeURIComponent(Cypress.config().baseUrl)}`)
                 .then(() => {
@@ -24,8 +21,10 @@ describe('Vote for a card', () => {
         });
 
         describe('on vote for a card', () => {
-            it("displays the votes", () => {
+            beforeEach(() => {
                 cy.voteCard();
+            });
+            it("displays the votes", () => {
                 cy.get('[data-cy="vote-card"]').should('not.exist');
                 cy.get('[data-cy="vote"]').should('exist');
             });
