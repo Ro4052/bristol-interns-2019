9e29c1bca9f88856fd391ffeb9a45bf5a02b1847
Matilda Woodfield <mwoodfield@scottlogic.com>
2019-07-22 12:36:09 +0100

    Removed uneeded code

--- src/components/shared/Auth/Auth.jsx
+++ src/components/shared/Auth/Auth.jsx
@@ -1,24 +1,12 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { authenticateUser, setPlayCard, setPlayWord } from '../../../store/playerActions';
+import { authenticateUser } from '../../../store/playerActions';
 
 export class Auth extends React.Component {
 
     componentDidMount() {
         this.props.authenticateUser();
-        this.instructPlayerOnRefresh();
-    }
-
-    instructPlayerOnRefresh() {
-        if (this.props.currentPlayer) {
-            if (this.props.status === "WAITING_FOR_CURRENT_PLAYER") {
-                this.props.setPlayCard(this.props.playedCard === 0 && !this.props.finishedRound && this.props.currentPlayer.username === this.props.cookie);
-            } else {
-                this.props.setPlayCard(this.props.playedCard === 0 && !this.props.finishedRound);
-            }
-            this.props.setPlayWord(this.props.currentPlayer.username === this.props.cookie && this.props.status === "WAITING_FOR_CURRENT_PLAYER" && !this.props.myWord && !this.props.finishedRound);
-        }
     }
 
     render() {
@@ -28,18 +16,11 @@ export class Auth extends React.Component {
 }
 
 const mapStateToProps = (state) => ({
-    cookie: state.playerReducer.cookie,
-    currentPlayer: state.gameReducer.currentPlayer,
-    status: state.gameReducer.status,
-    finishedRound: state.gameReducer.finishedRound,
-    playedCard: state.playerReducer.playedCard,
-    myWord: state.playerReducer.myWord
+    cookie: state.playerReducer.cookie
 });
 
 const mapDispatchToProps = (dispatch) => ({
-    authenticateUser: () => dispatch(authenticateUser()),
-    setPlayWord: (bool) => dispatch(setPlayWord(bool)),
-    setPlayCard: (bool) => dispatch(setPlayCard(bool))
+    authenticateUser: () => dispatch(authenticateUser())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Auth);
