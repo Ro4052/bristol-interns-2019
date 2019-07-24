2136983f0ae27921f1c6cb91f942307e93514e34
Matilda Woodfield <mwoodfield@scottlogic.com>
2019-07-24 11:53:19 +0100

    Possible test fix

--- cypress/integration/my_cards_spec.js
+++ cypress/integration/my_cards_spec.js
@@ -12,8 +12,9 @@ describe('My Cards', () => {
 
     describe('player clicks on a card', () => {
         it("its highlighted by being moved upwards on the y axis", () => {
-            cy.get('[data-cy="my-cards"] [data-cy="card"]').first().click();
-            cy.get('[data-cy="my-cards"]').children().first().should('have.css', "transform", "matrix(1, 0, 0, 1, 0, -30)");
+            cy.get('[data-cy="my-cards"] [data-cy="card"]').first().click().then(() => {
+                cy.get('[data-cy="my-cards"]').children().first().should('have.css', "transform", "matrix(1, 0, 0, 1, 0, -30)");
+            });
         });
     });
 });
