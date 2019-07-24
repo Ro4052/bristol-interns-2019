08a165b30e6dec5ae4b10ab8d2aa0b7522008a9d
Matilda Woodfield <mwoodfield@scottlogic.com>
2019-07-24 17:01:03 +0100

    Removed absolute placement of logout button

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import Button from '../shared/Button/Button';
 import { connect } from 'react-redux';
 import { logOutUser } from '../shared/Auth/AuthActions';
-import styles from './Logout.module.css';
 
 export class Logout extends React.Component {
     render() {
         return (
-            <div className={styles.logoutButton}>
+            <div>
                 <Button cy="logout" handleClick={this.props.logOutUser} text="Log out" />
             </div>
         );
