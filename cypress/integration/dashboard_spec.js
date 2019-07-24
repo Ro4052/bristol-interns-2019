dddc1cfc04d4751faab7f836d6784e10442ba737
Matilda Woodfield <mwoodfield@scottlogic.com>
2019-07-24 17:15:36 +0100

    Removed redundant tests

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -5,7 +5,6 @@ describe('Dashboard', () => {
         it("should display start button and shouldn't display any round information", () => {
             cy.get('[data-cy="start-game"]').should('exist');
             cy.get('[data-cy="round-number"]').should('not.exist');
-            cy.get('[data-cy="current-player"]').should('not.exist');
             cy.get('[data-cy="current-word"]').should('not.exist');
         });
     });
@@ -15,7 +14,6 @@ describe('Dashboard', () => {
         it("should hide start button and display the round information", () => {
             cy.get('[data-cy="start-game"]').should('not.exist');
             cy.get('[data-cy="round-number"]').should('exist');
-            cy.get('[data-cy="current-player"]').should('exist');
             cy.get('[data-cy="current-word"]').should('not.exist');
         });
     });
