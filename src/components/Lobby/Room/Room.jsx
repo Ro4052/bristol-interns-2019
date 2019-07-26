fa6fe04dffca3ec21c541ee76d6bc76a98eeffab
Matilda Woodfield <mwoodfield@scottlogic.com>
2019-07-26 08:46:05 +0100

    Room unit tests

--- src/components/Lobby/Room/Room.jsx
+++ src/components/Lobby/Room/Room.jsx
@@ -7,7 +7,7 @@ import { joinRoom, leaveRoom } from '../LobbyActions';
 
 export class Room extends React.Component {
     render() {
-        const waiting = (<>Waiting for {this.props.room.minPlayers - this.props.room.players.length} more players</>);
+        const waiting = <span data-cy="players-needed">Waiting for {this.props.room.minPlayers - this.props.room.players.length} more players</span>;
         const inRoom = this.props.room.players.some(player => player.username === this.props.username);
         return (
             <li className={styles.singleRoom} key={this.props.room.roomId} data-cy="room">
@@ -15,11 +15,12 @@ export class Room extends React.Component {
                 <ul id="players" data-cy='room-players'>
                     {this.props.room.players.map((player, key) => <li key={key}><span data-cy='player-username'>{player.username}</span></li>)}
                 </ul>
-                {!this.props.started && (this.props.room.minPlayers - this.props.room.players.length ? waiting : <StartGame />)}
-                {!this.props.started && inRoom ?
+                {!this.props.room.started && inRoom && !(this.props.room.minPlayers - this.props.room.players.length) && <StartGame />}
+                {!this.props.room.started && this.props.room.minPlayers - this.props.room.players.length && waiting}
+                {!this.props.room.started && (inRoom ?
                     <Button cy="leave-room" handleClick={() => this.props.leaveRoom(this.props.room.roomId)} text="Leave room" /> :
                     <Button cy="join-room" handleClick={() => this.props.joinRoom(this.props.room.roomId)} text="Join room" />
-                }
+                )}
             </li>
         );
     }
--- src/components/Lobby/Room/Room.test.js
+++ src/components/Lobby/Room/Room.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { Room } from './Room';
 import StartGame from '../../StartGame/StartGame';
 
@@ -35,10 +35,18 @@ describe('on given list of players', () => {
 });
 
 describe('on not started and enough players', () => {
-    // TODO: Only if you've joined the room
-    it('displays the start game button', () => {
-        const wrapper = shallow(<Room room={readyToStart} />);
-        expect(wrapper.exists(StartGame)).toEqual(true);
+    describe('if in room', () => {
+        it('displays the start game button', () => {
+            const wrapper = shallow(<Room room={readyToStart} username='player1' />);
+            expect(wrapper.exists(StartGame)).toEqual(true);
+        });
+    });
+
+    describe('if not in room', () => {
+        it('displays the start game button', () => {
+            const wrapper = shallow(<Room room={readyToStart} username='player3' />);
+            expect(wrapper.exists(StartGame)).toEqual(false);
+        });
     });
 });
 
@@ -49,51 +57,69 @@ describe('if not enough players and not started', () => {
     });
     
     it('shows how many players are needed', () => {
-        // TODO
+        const wrapper = shallow(<Room room={notEnoughPlayers} />);
+        expect(wrapper.find({ 'data-cy': 'players-needed' }).text()).toContain('1');
     });
 });
 
 describe('if already started', () => {
     it("doesn't display the start button", () => {
         const wrapper = shallow(<Room room={alreadyStarted} />);
-        // expect(wrapper.exists(StartGame)).toEqual(false);
+        expect(wrapper.exists(StartGame)).toEqual(false);
     });
 
     it("doesn't show waiting for players", () => {
         const wrapper = shallow(<Room room={alreadyStarted} />);
-        // TODO
+        expect(wrapper.exists({ 'data-cy': 'players-needed' })).toEqual(false);
     });
 
     it("doesn't display the join or leave room buttons", () => {
-        const wrapper = shallow(<Room room={alreadyStarted} />);
-        // TODO
+        const wrapper = mount(<Room room={alreadyStarted} />);
+        expect(wrapper.exists({ 'data-cy': 'join-room' })).toEqual(false);
+        expect(wrapper.exists({ 'data-cy': 'leave-room' })).toEqual(false);
     });
 });
 
 describe('if not started and not in room', () => {
     it('displays the join room button', () => {
-        const wrapper = shallow(<Room room={emptyRoom} username="player1" />);
-        // TODO
+        const wrapper = mount(<Room room={notEnoughPlayers} username="player2" />);
+        expect(wrapper.exists({ 'data-cy': 'join-room' })).toEqual(true);
+    });
+
+    it("doesn't display the leave room button", () => {
+        const wrapper = mount(<Room room={notEnoughPlayers} username="player2" />);
+        expect(wrapper.exists({ 'data-cy': 'leave-room' })).toEqual(false);
     });
 
     describe('on click the join room button', () => {
         it('calls joinRoom', () => {
-            const wrapper = shallow(<Room room={emptyRoom} username="player1" />);
-            // TODO
+            const joinRoom = jest.fn();
+            const wrapper = mount(<Room room={notEnoughPlayers} username="player2" joinRoom={joinRoom} />);
+            wrapper.find({ 'data-cy': 'join-room' }).simulate('click');
+            expect(joinRoom).toHaveBeenCalled();
+            joinRoom.mockRestore();
         });
     });
 });
 
 describe('if not started and in room', () => {
     it('displays the leave room button', () => {
-        const wrapper = shallow(<Room room={notEnoughPlayers} username="player1" />);
-        // TODO
+        const wrapper = mount(<Room room={notEnoughPlayers} username="player1" />);
+        expect(wrapper.exists({ 'data-cy': 'leave-room' })).toEqual(true);
+    });
+
+    it("doesn't display the join room button", () => {
+        const wrapper = mount(<Room room={readyToStart} username="player2" />);
+        expect(wrapper.exists({ 'data-cy': 'join-room' })).toEqual(false);
     });
 
     describe('on click the leave room button', () => {
         it('calls leaveRoom', () => {
-            const wrapper = shallow(<Room room={notEnoughPlayers} username="player1" />);
-            // TODO
+            const leaveRoom = jest.fn();
+            const wrapper = mount(<Room room={notEnoughPlayers} username="player1" leaveRoom={leaveRoom} />);
+            wrapper.find({ 'data-cy': 'leave-room' }).simulate('click');
+            expect(leaveRoom).toHaveBeenCalled();
+            leaveRoom.mockRestore();
         });
     });
 });
