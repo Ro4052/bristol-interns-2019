19e883ce6a052bbe13c6997066a685fab2a5e5a5
Matilda Woodfield <mwoodfield@scottlogic.com>
2019-07-26 09:59:57 +0100

    Finished writing integration tests for lobby

--- cypress/integration/lobby_spec.js
+++ cypress/integration/lobby_spec.js
@@ -1,32 +1,116 @@
 const url = Cypress.config().baseUrl;
 
 describe('Lobby', () => {
-    beforeEach(() => cy.login('unicorn'));
-
-    describe('when a player has successfully logged in', () => {
-        it('is able to create a new room and is then added to that room', () => {
+    describe('on clicking create room', () => {
+        beforeEach(() => {
+            cy.login('unicorn');
             cy.createRoom();
+        });
+
+        it('creates a new room and adds the player to that room', () => {
+            cy.get('[data-cy="current-rooms"]').children().should('have.length', 1);
             cy.get('[data-cy="room"]').should('exist');
-            cy.get('[data-cy="player-username"]').first().should('have.text', 'unicorn');
+            cy.get('[data-cy="player-username"]').contains('unicorn');
         });
 
-        it('is not able to create a room twice', () => {
-            cy.createRoom();
-            cy.get('[data-cy="room"]').first().should('exist');
-            cy.get('[data-cy="current-rooms"]').children().its('length').should('equal', 1);
+        it('displays the leave room button', () => {
+            cy.get('[data-cy="leave-room"]').should('exist');
         });
 
-        it('is able to join an existing room', () => {
-            cy.createRoom();
-            cy.request(`http://localhost:12346/connect?url=${encodeURIComponent(url)}`)
-            .then(() => cy.get('[data-cy="room-title"]'))
-            .then(($title) => {
-                const id = $title.text().split(" ")[1];
-                return cy.request(`http://localhost:12346/joinRoom?roomId=${id}&url=${encodeURIComponent(Cypress.config().baseUrl)}`);
-            })
-            .then(() => {
-                cy.get('[data-cy="player-username"]').first().should('have.text', 'unicorn');
-                cy.get('[data-cy="player-username"]').last().should('have.text', 'halfling');
+        it('displays how many players are needed to start the game', () => {
+            cy.get('[data-cy="players-needed"]').contains('1');
+        });
+
+        describe('on clicking leave room', () => {
+            beforeEach(() => cy.leaveRoom());
+
+            it('deletes the room', () => {
+                cy.get('[data-cy="current-rooms"]').children().should('have.length', 0);
+            });
+        });
+
+        describe('on another player joining the room', () => {
+            beforeEach(() => {
+                cy.request(`http://localhost:12346/connect?url=${encodeURIComponent(url)}`).then(() => {
+                    cy.request(`http://localhost:12346/joinRoom?roomId=0&url=${encodeURIComponent(Cypress.config().baseUrl)}`);
+                });
+            });
+
+            it('displays that player', () => {
+                cy.get('[data-cy="room-players"]').children().should('have.length', 2);
+                cy.get('[data-cy="player-username"]').contains('unicorn');
+                cy.get('[data-cy="player-username"]').contains('halfling');
+            });
+
+            it('displays the start game button', () => {
+                cy.get('[data-cy="start-game"]').should('exist');
+            });
+
+            describe('on clicking start game button', () => {
+                it('redirects to the dahsboard', () => {
+                    cy.startGame();
+                    cy.url().should('include', '/dashboard');
+                });
+            });
+        });
+    });
+
+    describe('on logging in when a room already exists', () => {
+        beforeEach(() => {
+            cy.request(`http://localhost:12346/connect?url=${encodeURIComponent(url)}`).then(() => {
+                cy.request(`http://localhost:12346/createRoom?url=${encodeURIComponent(url)}`).then(() => {
+                    cy.login('unicorn');
+                });
+            });
+        });
+
+        it('displays the existing room', () => {
+            cy.get('[data-cy="current-rooms"]').children().should('have.length', 1);
+            cy.get('[data-cy="room"]').should('exist');
+            cy.get('[data-cy="player-username"]').first().should('have.text', 'halfling');
+        });
+
+        it('displays the join room button', () => {
+            cy.get('[data-cy="join-room"]').should('exist');
+        });
+
+        describe('on clicking the join room button', () => {
+            beforeEach(() => cy.joinRoom());
+
+            it('adds the player to the room', () => {
+                cy.get('[data-cy="room-players"]').children().should('have.length', 2);
+                cy.get('[data-cy="player-username"]').contains('unicorn');
+                cy.get('[data-cy="player-username"]').contains('halfling');
+            });
+
+            describe('on clicking the create room button', () => {
+                beforeEach(() => cy.createRoom());
+
+                it('creates the room', () => {
+                    cy.get('[data-cy="current-rooms"]').children().should('have.length', 2);
+                    cy.get('[data-cy="room-title"]').contains('Room: 1');
+                });
+
+                it('adds you to the room', () => {
+                    cy.get('[data-cy="room"]').last().contains('unicorn');
+                });
+
+                it.skip('removes you from the other room', () => {
+                    cy.get('[data-cy="room"]').first().should('not.contain', 'unicorn');
+                });
+
+                describe.skip('on clicking join room on the first room', () => {
+                    beforeEach(() => cy.joinRoom());
+
+                    it('adds you to that room', () => {
+                        cy.get('[data-cy="room"]').first().contains('unicorn');
+                    });
+
+                    it('deletes your room', () => {
+                        cy.get('[data-cy="current-rooms"]').children().should('have.length', 1);
+                        cy.get('[data-cy="room"]').contains('Room: 0');
+                    });
+                });
             });
         });
     });
