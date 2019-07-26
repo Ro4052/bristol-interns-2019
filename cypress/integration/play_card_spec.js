3ea0625b3e6532dfe5184a17fa4713e27c363322
Matilda Woodfield <mwoodfield@scottlogic.com>
2019-07-26 10:48:36 +0100

    Fixed broken tests

--- cypress/integration/play_card_spec.js
+++ cypress/integration/play_card_spec.js
@@ -4,9 +4,17 @@ describe('Play card', () => {
     beforeEach(() => cy.login('unicorn'));
 
     describe('when first player has played card and word', () => {
-        it('is prompted to play a card', () => {
+        beforeEach(() => {
             cy.createRoom();
-            cy.startGame();
+            cy.request(`http://localhost:12346/connect?url=${encodeURIComponent(url)}`).then(() => {
+                cy.request(`http://localhost:12346/joinRoom?roomId=0&url=${encodeURIComponent(Cypress.config().baseUrl)}`).then(() => {
+                    cy.startGame();
+                    cy.request(`http://localhost:12346/playCardWord?url=${encodeURIComponent(Cypress.config().baseUrl)}`);
+                });
+            });
+        });
+
+        it('is prompted to play a card', () => {
             cy.get('[data-cy="play-card"]').should('exist')
         });
     });
--- cypress/integration/play_word_and_card_spec.js
+++ cypress/integration/play_word_and_card_spec.js
@@ -1,22 +1,47 @@
-describe('Play word and card', () => {
-    beforeEach(() => {
-        cy.login('unicorn')
-        cy.createRoom();
-    });
+const url = Cypress.config().baseUrl;
 
+describe('Play word and card', () => {
     describe('on start game', () => {
+        beforeEach(() => {
+            cy.login('unicorn');
+            cy.request(`http://localhost:12346/connect?url=${encodeURIComponent(url)}`).then(() => {
+                cy.request(`http://localhost:12346/createRoom?url=${encodeURIComponent(Cypress.config().baseUrl)}`).then(() => {
+                    cy.joinRoom();    
+                    cy.startGame();
+                });
+            });
+        });
+
         it('prompts user to play a word and a card', () => {
-            cy.startGame();
             cy.get('[data-cy="play-word"]').should('exist');
             cy.get('[data-cy="play-card"]').should('exist');
         });
-    });
 
-    describe('on send a word and a card', () => {
-        it('displays the word', () => {
-            cy.startGame();
-            cy.playCardWord();            
-            cy.get('[data-cy="current-word"]').should('have.text', 'word');
+        describe('player clicks on a card', () => {
+            beforeEach(() => {
+                cy.get('[data-cy="my-cards"]').find('[data-cy="card"]').first().click();
+            });
+            it("its class is updated to selected", () => {
+                cy.get('[data-cy="my-cards"]').find('[data-cy="card-wrapper"]').first().then(($wrapper) => {
+                    const selected = /selected/;
+                    const classList = Array.from($wrapper[0].classList);
+                    expect(classList.some(cls => selected.test(cls))).to.equal(true);
+                });
+            });
         });
-    });    
+
+        describe('on submit a bad word', () => {
+            it('displays an error', () => {
+                cy.sendWord();
+                cy.get('[data-cy="send-error"]').should('contain', 'Invalid word');
+            });
+        });   
+
+        describe('on send a word and a card', () => {
+            it('displays the word', () => {
+                cy.playCardWord();            
+                cy.get('[data-cy="current-word"]').should('have.text', 'word');
+            });
+        });  
+    });
 });
