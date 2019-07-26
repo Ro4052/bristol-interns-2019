aac9a2905ced800cc385bae6c2e6053bb19189b2
Sibela Chinareva <schinareva@scottlogic.com>
2019-07-26 16:41:21 +0100

    Added wait because of timeout

--- cypress/integration/play_word_and_card_spec.js
+++ cypress/integration/play_word_and_card_spec.js
@@ -19,10 +19,10 @@ describe('Play word and card', () => {
 
         describe('player clicks on a card', () => {
             beforeEach(() => {
+                cy.wait(1000); /*TODO: remove on MONDAY*/
                 cy.get('[data-cy="my-cards"]').find('[data-cy="card"]').first().click();
             });
             it("its class is updated to selected", () => {
-                cy.wait(1000); /*TODO: remove on MONDAY*/
                 cy.get('[data-cy="my-cards"]').find('[data-cy="card-wrapper"]').first().then(($wrapper) => {
                     const selected = /selected/;
                     const classList = Array.from($wrapper[0].classList);
