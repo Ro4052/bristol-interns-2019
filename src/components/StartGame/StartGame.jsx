d34e73c7c5342a8cd84d10895dbe57004c484ba4
Matilda Woodfield <mwoodfield@scottlogic.com>
2019-07-26 09:58:28 +0100

    Removed uneeded div

--- src/components/StartGame/StartGame.jsx
+++ src/components/StartGame/StartGame.jsx
@@ -15,9 +15,7 @@ export class StartGame extends React.Component {
 
     render() {
         return (
-            <div>
-                <Button cy="start-game" handleClick={this.startGame} text="Start game" />
-            </div>
+            <Button cy="start-game" handleClick={this.startGame} text="Start game" />
         );
     }
 }
