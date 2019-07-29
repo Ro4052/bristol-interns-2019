557357ecafe85691cf4e38094a0e4ec84ac3046b
Matilda Woodfield <mwoodfield@scottlogic.com>
2019-07-29 15:15:17 +0100

    Wrote e2e tests for joining a second game

--- cypress/integration/whole_game_spec.js
+++ cypress/integration/whole_game_spec.js
@@ -25,12 +25,29 @@ describe('Whole game', () => {
             }).then(() => {
                 cy.playCard();
                 cy.voteCard();
+                cy.newGame();
             });
         });
 
         it('redirects back to the lobby', () => {
-            cy.newGame();
             cy.url().should('include', '/lobby');
+        });
+
+        it('can create a new room', () => {
+            cy.createRoom();
+        });
+
+        it('can join an existing room', () => {
+            cy.request(`http://localhost:12346/createRoom?url=${encodeURIComponent(url)}`)
+            .then(() => cy.joinRoom());
+        });
+
+        it('can start a new game', () => {
+            cy.request(`http://localhost:12346/createRoom?url=${encodeURIComponent(url)}`)
+            .then(() => {
+                cy.joinRoom();
+                cy.startGame();
+            });
         });
     });
 });
