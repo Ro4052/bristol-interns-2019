838487c5e6bde6b7cba6495af3d6ed7aaa20a6d4
Matilda Woodfield <mwoodfield@scottlogic.com>
2019-08-01 09:56:13 +0100

    Rearranged test

--- cypress/integration/played_cards_spec.js
+++ cypress/integration/played_cards_spec.js
@@ -2,33 +2,25 @@ describe('Played cards', () => {
     const cardsNumber = 3;
     const numberOfPlayers = 2;
 
-    describe("at end of round", () => {
+    describe("when the storyteller has played", () => {
         beforeEach(() => {
             cy.login('unicorn');
             cy.request(`http://localhost:12346/connect?url=${encodeURIComponent(Cypress.config().baseUrl)}`)
-            .then(() => {
-                cy.createRoom();
-                return cy.get('[data-cy="room-title"]')
-            })
-            .then(($title) => {
-                const id = $title.text().split(" ")[1];
-                return cy.request(`http://localhost:12346/joinRoom?roomId=${id}&url=${encodeURIComponent(Cypress.config().baseUrl)}`)
-            })
-            .then(() => cy.startGame());
+            .then(() => cy.createRoom())
+            .then(() => cy.get('[data-cy="room-title"]'))
+            .then(() => cy.request(`http://localhost:12346/joinRoom?roomId=0&url=${encodeURIComponent(Cypress.config().baseUrl)}`))
+            .then(() => cy.startGame())
+            .then(() => cy.request(`http://localhost:12346/playCardWord?url=${encodeURIComponent(Cypress.config().baseUrl)}`));
         });
 
         it('a hidden card is displayed after first player plays', () => {
-            cy.request(`http://localhost:12346/playCardWord?url=${encodeURIComponent(Cypress.config().baseUrl)}`)
-            .then(() => cy.get('[data-cy="played-cards"]').children().its('length').should('eq', 1));
+            cy.get('[data-cy="played-cards"]').children().its('length').should('eq', 1);
         });
 
         it('all the played cards are displayed', () => {
-            cy.request(`http://localhost:12346/playCardWord?url=${encodeURIComponent(Cypress.config().baseUrl)}`)
-            .then(() => {
-                cy.playCard();
-                cy.get('[data-cy="my-cards"]').children().its('length').should('eq', cardsNumber - 1);
-                cy.get('[data-cy="played-cards"]').children().its('length').should('eq', numberOfPlayers);
-            });
+            cy.playCard();
+            cy.get('[data-cy="my-cards"]').children().its('length').should('eq', cardsNumber - 1);
+            cy.get('[data-cy="played-cards"]').children().its('length').should('eq', numberOfPlayers);
         });
     });
 });
