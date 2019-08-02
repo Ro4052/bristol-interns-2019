38285cc7d28d0016460b96f9eb5b31ff7f242a32
Matilda Woodfield <mwoodfield@scottlogic.com>
2019-08-02 16:38:43 +0100

    Fix

--- src/components/RoundCount/RoundCount.jsx
+++ src/components/RoundCount/RoundCount.jsx
@@ -7,8 +7,8 @@ export class RoundCount extends React.Component {
     render() {
         const options = [3, 4, 5, 6];
         return (
-            <select defaultValue={"How many rounds in your game?"} className={styles.roundCountBox} data-cy="num-rounds-options" onChange={event => this.props.setRoundCount(event.target.value)}>
-                <option disabled >How many rounds in your game?</option>
+            <select defaultValue="How many rounds in your game?" className={styles.roundCountBox} data-cy="num-rounds-options" onChange={event => this.props.setRoundCount(event.target.value)}>
+                <option disabled>How many rounds in your game?</option>
                 {options.map((option, key) => <option key={key} value={option}>{option}</option>)}
             </select>
         );
