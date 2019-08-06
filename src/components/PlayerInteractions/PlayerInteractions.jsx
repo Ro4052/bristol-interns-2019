99d8ea974f27adbacc46e7067e02d106f48be732
Isabelle Atkins <iatkins@scottlogic.com>
2019-08-06 14:22:03 +0100

    clearer prompts

--- src/components/PlayerInteractions/PlayerInteractions.jsx
+++ src/components/PlayerInteractions/PlayerInteractions.jsx
@@ -13,8 +13,9 @@ export class PlayerInteractions extends React.Component {
         return (
             <div className={cx(styles.playerInteractions, {votePrompt: this.props.voteCard})}>
                 {this.props.currentWord !== '' && <h2 className={styles.word} id="message">Word: <span data-cy='current-word'>"{this.props.currentWord}"</span></h2>}
-                {this.props.playCard && !this.props.playedCardId && <Prompt cy="play-card" text="Pick a card" />}
-                {this.props.voteCard && <Prompt cy="vote-card" text="Vote for a card" />}
+                {this.props.playCard && !this.props.playedCardId && !this.props.currentWord && <Prompt cy="play-card" text="Pick a card" />}
+                {this.props.playCard && !this.props.playedCardId && this.props.currentWord && <Prompt cy="play-card" text="Pick a card that the word best fits" />}
+                {this.props.voteCard && <Prompt cy="vote-card" text="Vote for the card that the word best fits" />}
                 {this.props.word && this.props.playedCardId && !this.props.finishedRound && <Button cy="end-turn" handleClick={this.props.endTurn} text="End my turn" />}
             </div>
         );
