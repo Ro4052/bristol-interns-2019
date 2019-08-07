712179883e5c73f9decf57043e603cea97584e5c
Isabelle Atkins <iatkins@scottlogic.com>
2019-08-07 16:34:49 +0100

    attempt

--- src/components/PlayerInteractions/PlayerInteractions.jsx
+++ src/components/PlayerInteractions/PlayerInteractions.jsx
@@ -12,7 +12,7 @@ export class PlayerInteractions extends React.Component {
             <div className={cx(styles.playerInteractions, {votePrompt: this.props.voteCard})}>
                 {this.props.status === 'WAITING_FOR_CURRENT_PLAYER' && !this.props.playCard && !this.props.voteCard &&<h2 data-cy='wait-for-storyteller' className={styles.justWait}>Waiting for the storyteller to play their turn</h2>}
                 {this.props.currentWord !== '' && <h2 className={styles.word} id="message">Word: <span data-cy='current-word'>"{this.props.currentWord}"</span></h2>}
-                {this.props.playCard && !this.props.playedCardId && (this.props.currentWord ? "Pick a card that the word best fits" : "Pick a card")}
+                {this.props.playCard && !this.props.playedCardId && <Prompt cy="play-card" text={(this.props.currentWord ? "Pick a card that the word best fits" : "Pick a card")} />}
                 {this.props.voteCard && <Prompt cy="vote-card" text="Vote for the card that the word best fits" />}
             </div>
         );
