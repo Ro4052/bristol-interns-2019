38dead1b1d123ba6f5be627df16ebe068ba911bc
Sibela Chinareva <schinareva@scottlogic.com>
2019-08-12 14:46:41 +0100

    Waiting for lobby to load first in test

--- cypress/integration/leaderboard_spec.js
+++ cypress/integration/leaderboard_spec.js
@@ -6,11 +6,11 @@ describe('Leaderboard', () => {
     describe('on logging in for the first time', () => {
         beforeEach(() => {
             cy.login('unicorn')
-            .then(() => cy.url().should('contain', 'lobby'))
-            .then(() => cy.get('[data-cy="go-leaderboard"]').click());
+            .then(() => cy.url().should('include', 'lobby'));
         });
 
         it('displays the username with a score of zero', () => {
+            cy.get('[data-cy="go-leaderboard"]').click()
             cy.get('[data-cy="player-username"]').contains('unicorn');
             score = cy.get('[data-cy="player-score"]').first().then($score => {
                 score = $score.text();
@@ -27,11 +27,11 @@ describe('Leaderboard', () => {
             .then(() => cy.startGame())
             .then(() => cy.playCardWord())
             .then(() => cy.get('[data-cy="game-over"]', { timeout: 20000 }))
-            .then(() => cy.newGame())
-            .then(() => cy.get('[data-cy="go-leaderboard"]').click());
+            .then(() => cy.newGame());
         });
 
-        it('updates the score of the player', () => {            
+        it('updates the score of the player', () => {
+            cy.get('[data-cy="go-leaderboard"]').click();  
             cy.get('[data-cy="player-score"]').first().should('contain', (parseInt(score) + 3).toString());
         });
     });
