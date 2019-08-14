b44d6c2ef4f19fe1e8e79f6cfc419bcc0cbf846e
Sibela Chinareva <schinareva@scottlogic.com>
2019-08-14 17:10:17 +0100

    Added some speech bubbles

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -31,18 +31,16 @@ export class Dashboard extends React.Component {
                         <Chat />
                     </div>
                     <div className={styles.middle}>
+                        <div className={styles.interactions}>
+                            {showPlayerInteractions && <PlayerInteractions />}
+                        <Timothy />
+                        </div>
                         <div className={styles.centerBox}>
                             {showPlayWord && <PlayWord />}
                             {(this.props.winner || this.props.drawers.length > 1) && <GameOver />}
                             {this.props.status !== statusTypes.GAME_OVER && <PlayedCards />}
                         </div>
                         {this.props.status !== statusTypes.NOT_STARTED && this.props.status !== statusTypes.GAME_OVER && <MyCards />}
-                    </div>
-                    <div className={styles.side}>
-                        <div className={styles.interactions}>
-                            {showPlayerInteractions && <PlayerInteractions />}
-                        </div>
-                        <Timothy />
                     </div>
                 </div>
             </div>
