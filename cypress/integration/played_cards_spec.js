0706aff3749ff2c262c1e5de3faf2a03e38283c2
Sibela Chinareva <schinareva@scottlogic.com>
2019-08-15 16:38:16 +0100

    Added timeout for played cards

--- cypress/integration/played_cards_spec.js
+++ cypress/integration/played_cards_spec.js
@@ -1,6 +1,6 @@
 describe('Played cards', () => {
     const numberOfPlayers = 2;
-
+    
     describe("when the storyteller has played", () => {
         beforeEach(() => {
             cy.login('unicorn');
@@ -18,7 +18,7 @@ describe('Played cards', () => {
         it('all the played cards are displayed', () => {
             cy.playCard();
             cy.get('[data-cy="played-card"]');
-            cy.get('[data-cy="played-cards"] [data-cy="card-wrapper"]').its('length').should('eq', numberOfPlayers - 1);
+            cy.get('[data-cy="played-cards"] [data-cy="card-wrapper"]', { timeout: 5000 }).its('length').should('eq', numberOfPlayers - 1);
         });
     });
 });
