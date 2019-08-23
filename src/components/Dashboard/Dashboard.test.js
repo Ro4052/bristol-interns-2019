896f4f3edbd8b54906d6048c939ab7ff11f40562
Matilda Woodfield <mwoodfield@scottlogic.com>
2019-08-23 13:58:20 +0100

    Updated client tests

--- src/components/Dashboard/Dashboard.test.js
+++ src/components/Dashboard/Dashboard.test.js
@@ -7,11 +7,11 @@ import PlayerInteractions from '../PlayerInteractions/PlayerInteractions';
 import PlayWord from '../PlayWord/PlayWord';
 
 const authenticateUserMock = jest.fn();
-const drawers = [];
+const winners = [{ username: 'player1' }];
 
 describe('on initial render', () => {
     it('calls authenticateUser', () => {
-        shallow(<Dashboard playedCards={[]} authenticateUser={authenticateUserMock} drawers={drawers}/>);
+        shallow(<Dashboard playedCards={[]} authenticateUser={authenticateUserMock} winners={[]}/>);
         expect(authenticateUserMock).toHaveBeenCalled();
         authenticateUserMock.mockRestore();
     });
@@ -19,56 +19,56 @@ describe('on initial render', () => {
 
 describe('on playCard false', () => {
     it("doesn't display the cards", () => {
-        const wrapper = shallow(<Dashboard playedCards={[]} playCard={false} authenticateUser={authenticateUserMock} drawers={drawers} />);
+        const wrapper = shallow(<Dashboard playedCards={[]} playCard={false} authenticateUser={authenticateUserMock} winners={[]} />);
         expect(wrapper.exists(MyCards)).toEqual(false);
     });
 });
 
 describe('on playCard true', () => {
     it('displays cards', () => {
-        const wrapper = shallow(<Dashboard playedCards={[]} playCard={true} authenticateUser={authenticateUserMock} drawers={drawers}/>);
+        const wrapper = shallow(<Dashboard playedCards={[]} playCard={true} authenticateUser={authenticateUserMock} winners={[]}/>);
         expect(wrapper.exists(MyCards)).toEqual(true);
     });
 });
 
-describe('on winner', () => {
+describe('on winners', () => {
     it('displays GameOver', () => {
-        const wrapper = shallow(<Dashboard playedCards={[]} winner={{ username: 'username' }} authenticateUser={authenticateUserMock} drawers={drawers}/>);
+        const wrapper = shallow(<Dashboard playedCards={[]} authenticateUser={authenticateUserMock} winners={winners}/>);
         expect(wrapper.exists(GameOver)).toEqual(true);
     })
 });
 
 describe('on no winner', () => {
     it("doesn't display GameOver", () => {
-        const wrapper = shallow(<Dashboard playedCards={[]} authenticateUser={authenticateUserMock} drawers={drawers}/>);
+        const wrapper = shallow(<Dashboard playedCards={[]} authenticateUser={authenticateUserMock} winners={[]}/>);
         expect(wrapper.exists(GameOver)).toEqual(false);
     });
 });
 
 describe('on play card', () => {
     it('displays player interaction', () => {
-        const wrapper = shallow(<Dashboard playedCards={[]} playCard={true} authenticateUser={authenticateUserMock} drawers={drawers}/>);
+        const wrapper = shallow(<Dashboard playedCards={[]} playCard={true} authenticateUser={authenticateUserMock} winners={[]}/>);
         expect(wrapper.exists(PlayerInteractions)).toEqual(true);
     });
 });
 
 describe('on play word', () => {
     it('displays play word interaction', () => {
-        const wrapper = shallow(<Dashboard playedCards={[]} playWord={true} playedCardId={1} authenticateUser={authenticateUserMock} drawers={drawers}/>);
+        const wrapper = shallow(<Dashboard playedCards={[]} playWord={true} playedCardId={1} authenticateUser={authenticateUserMock} winners={[]}/>);
         expect(wrapper.exists(PlayWord)).toEqual(true);
     });
 });
 
 describe('on vote card', () => {
     it('displays player interaction', () => {
-        const wrapper = shallow(<Dashboard playedCards={[]} voteCard={true} playCard={true} authenticateUser={authenticateUserMock} drawers={drawers}/>);
+        const wrapper = shallow(<Dashboard playedCards={[]} voteCard={true} playCard={true} authenticateUser={authenticateUserMock} winners={[]}/>);
         expect(wrapper.exists(PlayerInteractions)).toEqual(true);
     });
 });
 
 describe('on end of turn', () => {
     it('displays player interaction', () => {
-        const wrapper = shallow(<Dashboard playedCards={[]} finishedRound={false} username={'username'} currentPlayer={{ username: 'username' }} word={true} playedCardId={1} authenticateUser={authenticateUserMock} drawers={drawers}/>);
+        const wrapper = shallow(<Dashboard playedCards={[]} finishedRound={false} username={'username'} currentPlayer={{ username: 'username' }} word={true} playedCardId={1} authenticateUser={authenticateUserMock} winners={[]}/>);
         expect(wrapper.exists(PlayerInteractions)).toEqual(true);
     });
 });
