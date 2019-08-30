4532ea36a8c772a28501d521c57aaa1b036143a4
Ro4052 <Ro4052@users.noreply.github.com>
2019-08-30 10:57:27 +0100

    fix story teller timeout value

--- server/models/GameLogic.js
+++ server/models/GameLogic.js
@@ -8,7 +8,7 @@ const db = (process.env.NODE_ENV === 'testing') ? require('../queries/testdb') :
 // Set durations
 const promptDuration = process.env.NODE_ENV === 'testing' ? 4000 : 30000;
 const voteDuration = process.env.NODE_ENV === 'testing' ? 5000 : 30000;
-const storytellerDuration = process.env.NODE_ENV === 'testing' ? 4000 : 2000;
+const storytellerDuration = process.env.NODE_ENV === 'testing' ? 4000 : 60000;
 const nextRoundDuration = process.env.NODE_ENV === 'testing' ? 3000 : 10000;
 const minPlayers = process.env.NODE_ENV === 'testing' ? 1 : 3;
 const maxPlayers = 6;
