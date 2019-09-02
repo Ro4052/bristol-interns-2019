0e9fa2d5b108f8e8a86a0e267150748c7ab21085
Matilda Woodfield <mwoodfield@scottlogic.com>
2019-09-02 16:50:21 +0100

    functional components

--- src/components/Leaderboard/LeaderboardButton/LeaderboardButton.jsx
+++ src/components/Leaderboard/LeaderboardButton/LeaderboardButton.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-export class LeaderboardButton extends React.Component {
-    render() {
-        return (
-            <div>
-                <button onClick={() => this.props.history.push('/leaderboard')} data-cy="go-leaderboard" type='button'>Leaderboard</button>
-            </div>
-        );
-    }
+export function LeaderboardButton(props) {
+    return (
+        <div>
+            <button onClick={() => props.history.push('/leaderboard')} data-cy="go-leaderboard" type='button'>Leaderboard</button>
+        </div>
+    );
 }
 
 export default LeaderboardButton;
--- src/components/Upload/UploadButton/UploadButton.jsx
+++ src/components/Upload/UploadButton/UploadButton.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 
-export class UploadButton extends React.Component {
-    goToUpload() {
-        
-    }
-
-    render() {
-        return (
-            <div>
-                <button onClick={() => this.props.history.push('/upload')} data-cy="go-upload" type='button'>Upload</button>
-            </div>
-        );
-    }
+export function UploadButton(props) {
+    return (
+        <div>
+            <button onClick={() => props.history.push('/upload')} data-cy="go-upload" type='button'>Upload</button>
+        </div>
+    );
 }
 
 export default UploadButton;
