f52f694a44cbca6998379104e8fc5e42ab39b92b
Matilda Woodfield <mwoodfield@scottlogic.com>
2019-09-04 11:26:02 +0100

    hookified player interactions

--- src/components/PlayerInteractions/PlayerInteractions.jsx
+++ src/components/PlayerInteractions/PlayerInteractions.jsx
@@ -1,62 +1,36 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import Timer from '../Timer/Timer';
+import { useSelector, useDispatch } from 'react-redux';
 import styles from './PlayerInteractions.module.css';
 import classNames from 'classnames/bind';
-import { setVoteCardTimer, setPlayCardTimer, setStorytellerTimer, requestNextRound } from '../Timer/TimerActions';
+import { requestNextRound } from '../Timer/TimerActions';
 import { statusTypes } from '../../services/statusTypes';
 
-const cx = classNames.bind(styles);
+export function PlayerInteractions() {
+    const { status, currentWord } = useSelector(state => state.dashboardReducer);
+    const { username } = useSelector(state => state.authReducer);
+    const { currentPlayer } = useSelector(state => state.playersReducer);
+    const { playCard, playedCardId } = useSelector(state => state.myCardsReducer);
+    const { voteCard } = useSelector(state => state.playedCardsReducer);
+    const { playCardDuration, voteCardDuration } = useSelector(state => state.timerReducer);
 
-export class PlayerInteractions extends React.Component {
-    render() {
-        return (
-            <div className={cx(styles.playerInteractions, {votePrompt: this.props.voteCard})}>
-                <div className={styles.text}>
-                    {this.props.currentWord && (
-                        this.props.username === this.props.currentPlayer.username ?
-                        <span>You picked the word <span data-cy="current-word">"{this.props.currentWord}"</span>. </span> :
-                        <span>The storyteller picked the word <span data-cy="current-word">"{this.props.currentWord}"</span>. </span>
-                    )}
-                    {this.props.status === statusTypes.WAITING_FOR_CURRENT_PLAYER && !this.props.playCard && !this.props.voteCard && <span data-cy='wait-for-storyteller' className={styles.justWait}>Waiting for the storyteller...</span>}
-                    {this.props.playCard && !this.props.playedCardId && this.props.currentWord && <span data-cy="play-card">Pick a card from your hand that this word describes.</span>}
-                    {this.props.playCard && !this.props.playedCardId && !this.props.currentWord && <span data-cy="play-card">You are the storyteller! Pick a card from your hand.</span>}
-                    {this.props.voteCard && <span data-cy="vote-card">Vote on which card you think is the storyteller's one.</span>}
-                    {this.props.status === statusTypes.WAITING_FOR_OTHER_PLAYERS && this.props.playCardDuration > 0 && !this.props.playCard && <span data-cy='wait-for-cards' className={styles.justWait}>Waiting for other players to pick cards...</span>}
-                    {this.props.status === statusTypes.WAITING_FOR_VOTES && this.props.voteCardDuration > 0 && !this.props.voteCard && <span data-cy='wait-for-votes' className={styles.justWait}>Waiting for the other players to vote...</span>}
-                    {this.props.status === statusTypes.DISPLAY_ALL_VOTES && <button onClick={this.props.requestNextRound} data-cy='next-round'>Next round</button>}
-                    {this.props.error && <span data-cy='error'>Error: {this.props.error}</span>}
-                </div>
-                <div>
-                    {(this.props.status === statusTypes.WAITING_FOR_CURRENT_PLAYER && this.props.storytellerDuration > 0) && <Timer cy="storyteller-timer" setDuration={this.props.setStorytellerTimer} duration={this.props.storytellerDuration} />}
-                    {(this.props.status === statusTypes.WAITING_FOR_OTHER_PLAYERS && this.props.playCardDuration > 0) && <Timer cy="card-timer" setDuration={this.props.setPlayCardTimer} duration={this.props.playCardDuration} />}
-                    {(this.props.status === statusTypes.WAITING_FOR_VOTES && this.props.voteCardDuration > 0) && <Timer cy="vote-timer" setDuration={this.props.setVoteCardTimer} duration={this.props.voteCardDuration} />}
-                </div>
-            </div>
-        );
-    }
+    const dispatch = useDispatch();
+
+    const isStatus = checkStatus => status === checkStatus;
+    const isCurrentPlayer = ()  => currentPlayer.username === username;
+
+    return (
+        <div className={classNames(styles.playerInteractions)}>
+            {currentWord && isCurrentPlayer() && <span>You picked the word <span data-cy="current-word">"{currentWord}"</span>. </span>}
+            {currentWord && !isCurrentPlayer() && <span>The storyteller picked the word <span data-cy="current-word">"{currentWord}"</span>. </span>}
+            {isStatus(statusTypes.WAITING_FOR_CURRENT_PLAYER) && !isCurrentPlayer() && <span data-cy='wait-for-storyteller'>Waiting for the storyteller...</span>}
+            {playCard && !playedCardId && currentWord && <span data-cy="play-card">Pick a card from your hand that this word describes.</span>}
+            {playCard && !playedCardId && !currentWord && <span data-cy="play-card">You are the storyteller! Pick a card from your hand.</span>}
+            {voteCard && <span data-cy="vote-card">Vote on which card you think is the storyteller's one.</span>}
+            {isStatus(statusTypes.WAITING_FOR_OTHER_PLAYERS) && playCardDuration > 0 && !playCard && <span data-cy='wait-for-cards'>Waiting for other players to pick cards...</span>}
+            {isStatus(statusTypes.WAITING_FOR_VOTES) && voteCardDuration > 0 && !voteCard && <span data-cy='wait-for-votes'>Waiting for the other players to vote...</span>}
+            {isStatus(statusTypes.DISPLAY_ALL_VOTES) && <button onClick={() => dispatch(requestNextRound())} data-cy='next-round'>Next round</button>}
+        </div>
+    );
 }
 
-const mapStateToProps = state => ({
-    status: state.dashboardReducer.status,
-    playCard: state.myCardsReducer.playCard,
-    voteCard: state.playedCardsReducer.voteCard,
-    word: state.playWordReducer.word,
-    playedCardId: state.myCardsReducer.playedCardId,
-    currentWord: state.dashboardReducer.currentWord,
-    playCardDuration: state.timerReducer.playCardDuration,
-    voteCardDuration: state.timerReducer.voteCardDuration,
-    storytellerDuration: state.timerReducer.storytellerDuration,
-    error: state.timerReducer.error,
-    currentPlayer: state.playersReducer.currentPlayer,
-    username: state.authReducer.username
-});
-
-const mapDispatchToProps = dispatch => ({
-    setPlayCardTimer: duration => dispatch(setPlayCardTimer(duration)),
-    setVoteCardTimer: duration => dispatch(setVoteCardTimer(duration)),
-    setStorytellerTimer: duration => dispatch(setStorytellerTimer(duration)),
-    requestNextRound: () => dispatch(requestNextRound())
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(PlayerInteractions);
+export default PlayerInteractions;
--- src/components/PlayerInteractions/PlayerInteractions.test.js
+++ src/components/PlayerInteractions/PlayerInteractions.test.js
@@ -1,81 +1,49 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
-import { PlayerInteractions } from './PlayerInteractions';
+import { mount } from 'enzyme';
+import PlayerInteractions from './PlayerInteractions';
 import { statusTypes } from '../../services/statusTypes';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import * as actions from '../Timer/TimerActions';
 
-const middlewares = [];
+const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
-const state = {
-    dashboardReducer: {},
+const store = mockStore({
+    authReducer: {},
+    dashboardReducer: {
+        status: statusTypes.DISPLAY_ALL_VOTES
+    },
     myCardsReducer: {},
     playedCardsReducer: {},
     playWordReducer: {},
+    playersReducer: {},
     timerReducer: {}
-};
-const store = mockStore(state);
-
-describe('on play card flag', () => {
-    it('displays the play card prompt', () => {
-        const wrapper = shallow(<PlayerInteractions playCard={true} />);
-        expect(wrapper.find({ 'data-cy': 'play-card' }));
-    });
 });
 
-describe('on vote card flag', () => {
-    it('displays the vote card prompt', () => {
-        const wrapper = shallow(<PlayerInteractions voteCard={true} />);
-        expect(wrapper.find({ 'data-cy': 'vote-card' }));
-    });
-});
+describe('PlayerInteractions', () => {
+    let wrapper;
+    const setState = jest.fn();
+    jest.spyOn(React, 'useState').mockImplementation(init => [init, setState]);
+    const requestNextRoundSpy = jest.spyOn(actions, 'requestNextRound');
 
-describe('on waiting for storyteller', () => {
-    it('displays the waiting prompt', () => {
-        const wrapper = shallow(<PlayerInteractions status={statusTypes.WAITING_FOR_CURRENT_PLAYER} />)
-        expect(wrapper.exists({ 'data-cy': 'wait-for-storyteller' })).toEqual(true);
-    });
-});
-
-describe('on word having been submitted', () => {
-    it('displays the word', () => {
-        const wrapper = shallow(<PlayerInteractions currentPlayer={{}} playCard={true} currentWord="dog" />)
-        expect(wrapper.exists({ 'data-cy': 'current-word' })).toEqual(true);
-    });
-});
-
-describe('when you have voted are waiting for others to do so', () => {
-    it('tells you to wait', () => {
-        const wrapper = mount(<PlayerInteractions status={statusTypes.WAITING_FOR_VOTES} voteCardDuration={2} />);
-        expect(wrapper.exists({ 'data-cy': 'wait-for-votes' })).toEqual(true);
-    });
-});
-
-describe('when you have played your card and are waiting for others to do so', () => {
-    it('tells you to wait', () => {
-        const wrapper = mount(<PlayerInteractions status={statusTypes.WAITING_FOR_OTHER_PLAYERS} playCardDuration={2} />);
-        expect(wrapper.exists({ 'data-cy': 'wait-for-cards' })).toEqual(true);
-    });
-});  
-
-describe('when all the votes are displayed', () => {
-    it('displays the next round button', () => {
-        const wrapper = mount(<PlayerInteractions status={statusTypes.DISPLAY_ALL_VOTES} />);
-        expect(wrapper.exists({ 'data-cy': 'next-round' })).toEqual(true);
-    });
-    it('calls requestNextRound when button is clicked', () => {
-        const requestNextRound = jest.fn();
-        const wrapper = mount(
+    beforeEach(() => {
+        store.clearActions();
+        wrapper = mount(
             <Provider store={store}>
-                <PlayerInteractions status={statusTypes.DISPLAY_ALL_VOTES} requestNextRound={requestNextRound} />
+                <PlayerInteractions />
             </Provider>
         );
-        wrapper.find({ 'data-cy': 'next-round' }).simulate('click');
-        expect(requestNextRound).toHaveBeenCalled();
-        requestNextRound.mockRestore();
     });
-    it('displays an error if calling next round is not allowed', () => {
-        const wrapper = mount(<PlayerInteractions error={"Next round is not allowed."} />);
-        expect(wrapper.find({ 'data-cy': 'error' }).text()).toEqual("Error: Next round is not allowed.");
+
+    afterEach(() => {
+        jest.clearAllMocks();
     });
-});  
+
+    describe('on click next round button', () => {
+        it('dispatches requestNextRound', () => {
+            wrapper.find({ 'data-cy': 'next-round' }).props().onClick();
+            expect(requestNextRoundSpy).toHaveBeenCalled();
+        });
+    });
+});
