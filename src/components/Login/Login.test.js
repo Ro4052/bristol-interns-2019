ff30049f0260ac0bb0e657b7bb8e53655fe968a3
Matilda Woodfield <mwoodfield@scottlogic.com>
2019-09-04 15:46:04 +0100

    Working?

--- src/components/Login/Login.test.js
+++ src/components/Login/Login.test.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Login } from './Login';
-import { shallow, mount } from 'enzyme';
+import Login from './Login';
+import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import * as actions from './LoginActions';
 
-const middlewares = [];
+const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 const state = {
     gameOverReducer: { winners: [] },
@@ -13,39 +15,43 @@ const state = {
 };
 const store = mockStore(state);
 
-describe('on render', () => {
-    it('calls authenticateUser', () => {
-        const authenticateUser = jest.fn();
-        mount(
+describe('Login', () => {
+    let wrapper;
+    const setState = jest.fn();
+    jest.spyOn(React, 'useState').mockImplementation(init => [init, setState]);
+    const authenticateUserSpy = jest.spyOn(actions, 'authenticateUser');
+    const logInSpy = jest.spyOn(actions, 'logIn');
+
+    beforeEach(() => {
+        store.clearActions();
+        wrapper = mount(
             <Provider store={store}>
-                <Login authenticateUser={authenticateUser} />
+                <Login />
             </Provider>
         );
-        expect(authenticateUser).toHaveBeenCalled();
-        authenticateUser.mockRestore();
     });
-});
 
-describe('on type in input', () => {
-    it('updates the state', () => {
-        const wrapper = shallow(<Login authenticateUser={jest.fn()}/>);
-        wrapper.find({ 'data-cy': 'username' }).simulate('change', { preventDefault: () => {}, target: { value: 'username' } });
-        expect(wrapper.state().username).toEqual('username');
+    afterEach(() => {
+        jest.clearAllMocks();
     });
-});
 
-describe('on submit', () => {
-    it('calls the logIn function', () => {
-        const logIn = jest.fn();
-        const signUp = jest.fn();
-        const wrapper = mount(
-            <Provider store={store}>
-                <Login authenticateUser={jest.fn()} logIn={logIn} signUp={signUp} />
-            </Provider>
-        );
-        wrapper.find({ 'data-cy': 'username' }).simulate('change', { preventDefault: () => {}, target: { value: 'username' } });
-        wrapper.find({ 'data-cy': 'login-form' }).simulate('submit');
-        expect(logIn).toHaveBeenCalled();
-        logIn.mockRestore();
+    describe('on render', () => {
+        it('calls authenticateUser', () => {
+            expect(authenticateUserSpy).toHaveBeenCalled();
+        });
+    });
+    
+    describe('on type in input', () => {
+        it('updates the state', () => {
+            wrapper.find({ 'data-cy': 'username' }).simulate('change', { preventDefault: () => {}, target: { value: 'username' } });
+            expect(setState).toHaveBeenCalledWith('username');
+        });
+    });
+    
+    describe('on submit', () => {
+        it('calls the logIn function', () => {
+            wrapper.find({ 'data-cy': 'login-form' }).simulate('submit');
+            expect(logInSpy).toHaveBeenCalled();
+        });
     });
 });
